bd4914dd30e0ceb9a050c0b1cf6dfbe5fec99c61
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-08-05 15:08:12 +0500

    Add json file with login details for the python code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import json
  
 def on_connect(client, userdata, flags, rc):
     print("Connected to broker")
@@ -7,15 +8,19 @@ def on_message(client, userdata, message):
     print ("Message received: "  + message.payload.decode())
 
 if __name__ == "__main__":
-    ip = input("Enter Server IP: ").strip()
-    port = input("Enter Server port: ").strip()
-    username = input("Enter Username: ").strip()
-    password = input("Enter Password: ").strip()
-    channel = input("Enter Channel: ").strip()
+    f = open('login-details.json', 'r')
+    login_details = json.load(f)
+    f.close()
+
+    ip = login_details.get("ip", "").strip()
+    port = login_details.get("port", 0)
+    username = login_details.get("username", "").strip()
+    password = login_details.get("password", "").strip()
+    channel = login_details.get("channel", "").strip()
 
     client = mqtt.Client()
     client.username_pw_set(username, password=password)
-    client.connect(ip, int(port)) 
+    client.connect(ip, port) 
 
     client.on_connect = on_connect       #attach function to callback
     client.on_message = on_message       #attach function to callback
